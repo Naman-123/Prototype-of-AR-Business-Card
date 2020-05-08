cb9c1fc517ad6360d5cac5efe5384e2e2e261535
Naman-123 <namannjaj456@gmail.com>
2020-05-08 15:57:00 +0530

    some init msg

---
+++ Assets/MyAssets/Link.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+  public void facebook()
+    {
+        Application.OpenURL("https://www.facebook.com");
+    }
+    public void Twitter()
+    {
+        Application.OpenURL("https://twitter.com");
+    }
+    public void GitHub()
+    {
+        Application.OpenURL("https://github.com/");
+    }
+    public void LinkedIn()
+    {
+        Application.OpenURL("https://linkedin.com/");
+    }
+}
